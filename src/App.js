85ad4000ae788644c06c9fa24aab513fcac1b365
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-06-18 14:42:43 +0300

    choose file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [file, setFile] = useState();
+
   return (
     <>
       <h1>Notary App</h1>
-      <input type="file" />
+      <input type="file" onChange={ev => setFile(ev.target.files[0])} />
       <div>
         <button>Register</button>
         <button>Verify</button>
